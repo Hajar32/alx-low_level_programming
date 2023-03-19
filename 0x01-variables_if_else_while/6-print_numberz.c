45c4b4561ef30aed4a37bd4376b2d32e044ab29f
hajar <moustaouihajar06@gmail.com>
2023-03-19 10:37:17 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always (0)
+ */
+
+int main()
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
