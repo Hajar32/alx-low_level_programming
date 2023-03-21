713744d5674fc9077f38b0f8267b5a7e7fb2ca16
hajar <moustaouihajar06@gmail.com>
2023-03-21 13:39:34 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_sign - entry point
+ * print_sign - function that prints the sign of a number
  *
  * @n: integer
  *
- * Return: 1 is greater than zero, 0 is zero, -1 is less than zero 
+ * Return: 1 is greater than zero, 0 is zero, -1 is less than zero
  */
 int print_sign(int n)
 {
