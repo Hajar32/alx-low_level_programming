e917980eff9da2508c466689c21c5d84c7555488
hajar <moustaouihajar06@gmail.com>
2023-04-03 10:32:40 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,10 +5,10 @@
  * @dest: destination
  * @src: source
  * @n: bytes
- * return: dest
+ * Return: dest
  */
 
-char *-memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
