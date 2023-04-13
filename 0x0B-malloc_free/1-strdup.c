9dc15cf14a9eff0f8527918fa6c93d88a8182974
Hajar32 <moustaouihajar06@gmail.com>
2023-04-12 17:07:02 -0700

    Task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_strdup - return a pointer to a newly allocated space in memory
+ * *_strdup - returns a pointer to a newly allocated space in memory
  * @str: string
  * Return: pointer to the duplicated string, or NULL
  */
