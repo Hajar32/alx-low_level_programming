f4ce52033dbba3501f8657a7765f3e120f59fd42
Hajar32 <moustaouihajar06@gmail.com>
2023-04-13 13:53:06 -0700

    Task5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -29,10 +29,9 @@ char *_memset(char *s, char b, unsigned int n)
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p
-
-		if (nmemb == 0 || size == 0)
-			return (NULL);
+	char *p;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
