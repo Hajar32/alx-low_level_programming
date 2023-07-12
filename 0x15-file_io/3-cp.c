f19a092cf2988fab1028d1ec51fea09829aa87ba
Hajar32 <moustaouihajar06@gmail.com>
2023-07-12 13:49:52 -0700

    Task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@ void close_file(int fd);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name file buffer is storing chars for.
+ * @file: The namhe file buffer is storing chars for.
  *
  * Return: A pointer to the newly-allocated buffer.
  */
@@ -52,8 +52,8 @@ void close_file(int fd)
  * Return: 0 on success.
  *
  * Description: If the argument count is incorrect - exit code 97.
- *              If file_from does not exist pr cannot be read - exit code 98.
- *              If file_to cannot be created pr written to - exit code 99.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
  *              If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
