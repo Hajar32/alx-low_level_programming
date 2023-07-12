3d03d9a9fa08d1532c7679277c78ceb5d0ba5b91
Hajar32 <moustaouihajar06@gmail.com>
2023-07-12 08:50:10 -0700

    Task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,3 +21,14 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (x = 0; text_content[x];)
 			x++;
 	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, x);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
